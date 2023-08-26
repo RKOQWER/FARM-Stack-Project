38f0babac204cd4067b48702a88845d24bfea350
RKOQWER <rahul.parasmani@gmail.com>
2023-08-26 10:51:09 +0530

    Updating index.py

--- index.py
+++ index.py
@@ -2,9 +2,24 @@
 
 from fastapi import FastAPI
 from routes.student import student_router
+from fastapi.middleware.cors import CORSMiddleware
+
+client_apps=['http://localhost:3000'] # our react app will be running on this ip and port
+
+
 
 # create app
 app=FastAPI()
 
 # register your router
 app.include_router(student_router)
+
+# Register app with CORS middle ware to allow resource sharing between different domains/origins
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=client_apps,
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=['*']
+	)
